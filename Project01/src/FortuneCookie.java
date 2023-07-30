6318460c500030c064b6dc967700d81fda21c929
josi-lima <teacher.josi@yahoo.com>
2023-07-29 21:45:14 -0300

    :books: feat: if-else in java

---
+++ Project01/src/FortuneCookie.java
@@ -0,0 +1,11 @@
+public class FortuneCookie {
+  public static void main(String[] args) {
+    int luckyNum = 7;
+
+    if (luckyNum > 0 && luckyNum <= 20) {
+      System.out.println("Enjoy the good luck a friend brings you!");
+    } else {
+      System.out.println("Your shoe selection will make you happy today!");
+    }
+  }  
+}
