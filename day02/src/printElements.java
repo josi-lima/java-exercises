96511de447f08d73e110f3204df3c34a21861f34
josi-lima <teacher.josi@yahoo.com>
2023-07-31 14:26:33 -0300

    :books: feat: add function checkElements

---
+++ day02/src/printElements.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class printElements {
+    public static void main(String args[]) {    
+      // Create a new scanner object
+      Scanner scan = new Scanner(System.in); 
+
+      // Scan the integer which is in the first line of the input
+      int i = scan.nextInt(); 
+
+      // Scan the double which is on the second line
+      double d = scan.nextDouble(); 
+
+      /* 
+      * At this point, the scanner is still on the second line at the end
+      * of the double, so we need to move the scanner to the next line
+      * scans to the end of the previous line which contains the double. 
+      */
+      scan.nextLine();    
+
+      // reads the complete next line which contains the string sentence            
+      String s = scan.nextLine();  
+      
+      scan.close();
+
+      System.out.println("String: " + s);
+      System.out.println("Double: " + d);
+      System.out.println("Int: " + i);
+  }
+}
