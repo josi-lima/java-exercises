14f188915123a567313ed014fd939127f39d9751
josi-lima <teacher.josi@yahoo.com>
2023-07-30 22:30:41 -0300

    :recycle: refactor: refactoring function MealPrice

--- day02/src/mealPrice.java
+++ day02/src/mealPrice.java
@@ -1,15 +1,21 @@
 public class mealPrice {
 
-  public static void calculateMealPrice(double listedPrice, double tipRate, double taxRate) {
+  public static double calculateMealPrice(double listedPrice, double tipRate, double taxRate) {
     double tip = tipRate * listedPrice / 100;
     double tax = taxRate * listedPrice / 100;
     double result = listedPrice + tip + tax;
-    System.out.println("The price of your meal is $ " + listedPrice + ".");
-    System.out.println("The calculated tip is $ " + tip + ".");
-    System.out.println("The total price of your meal is $ " + result + ".");
+
+    System.out.println("The price of your meal is $" + listedPrice + ".");
+    System.out.println("The calculated tip is $" + tip + ".");
+    
+    return result;
   }
   
   public static void main(String[] args) {
-    calculateMealPrice(120, 10, 8);
+    double groupTotalMealCost = calculateMealPrice(120, 10, 8);
+    double individualCost = groupTotalMealCost / 5;
+
+    System.out.println("The total price of your meal is $" + groupTotalMealCost + ".");
+    System.out.println("The individual cost for each person is $" + individualCost + ".");
   }
 }
