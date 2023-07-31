079cd31035dfed7bf15fbf3decb6b4e9aa325392
josi-lima <teacher.josi@yahoo.com>
2023-07-31 14:44:16 -0300

    :recycle: refactor: turn function MealPrice more dynamic

--- day02/src/mealPrice.java
+++ day02/src/mealPrice.java
@@ -1,21 +1,21 @@
 public class mealPrice {
 
-  public static double calculateMealPrice(double listedPrice, double tipRate, double taxRate) {
+  public static double calculateMealPrice(int numberOfPeople, double listedPrice, double tipRate, double taxRate) {
     double tip = tipRate * listedPrice / 100;
     double tax = taxRate * listedPrice / 100;
     double result = listedPrice + tip + tax;
-
+    double individualCost = result / numberOfPeople ;
+ 
     System.out.println("The price of your meal is $" + listedPrice + ".");
     System.out.println("The calculated tip is $" + tip + ".");
+    System.out.println("The individual cost for each person is $" + individualCost + ".");
     
     return result;
   }
   
   public static void main(String[] args) {
-    double groupTotalMealCost = calculateMealPrice(120, 10, 8);
-    double individualCost = groupTotalMealCost / 5;
+    double groupTotalMealCost = calculateMealPrice(4, 120, 10, 8);
 
     System.out.println("The total price of your meal is $" + groupTotalMealCost + ".");
-    System.out.println("The individual cost for each person is $" + individualCost + ".");
   }
 }
