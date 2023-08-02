826a19d36b441f3bd71dcff1bbebe7e6fb167e40
josi-lima <teacher.josi@yahoo.com>
2023-08-01 23:15:35 -0300

    :recycle: refactor: turn function MealPrice more dynamic

--- day03/src/CelsiusToFahrenheit.java
+++ day03/src/CelsiusToFahrenheit.java
@@ -1,14 +1,14 @@
 public class CelsiusToFahrenheit {
 
-  // 🌱 8. Create a function that will convert from Celsius to Fahrenheit.
+  // 🌱 8. Create a function that will convert degrees from Celsius to Fahrenheit.
   // Reminder: C = F – 32 / 1.8
 
-  public static double checkCelsiusToFahrenheit(double degreeCelsius) {
+  public static double changeCelsiusToFahrenheit(double degreeCelsius) {
     return degreeCelsius * 1.8 + 32;
   }
   
   public static void main(String[] args) {
-    double degreeFahrenheit = checkCelsiusToFahrenheit(20);
-    System.out.println(degreeFahrenheit);
+    double degreeFahrenheit = changeCelsiusToFahrenheit(20);
+    System.out.println("Temperature in Fahrenheit: " + degreeFahrenheit + " degrees.");
   }
 }
