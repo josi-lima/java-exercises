b62dca5288a29dcfd5a51d83274325ed9ca5accd
josi-lima <teacher.josi@yahoo.com>
2023-08-05 12:59:24 -0300

    :bulb: docs: add comments to class TriangleApp

--- day05/src/Main.java
+++ day05/src/Main.java
@@ -1,15 +1,29 @@
 public class Main {
 
   public static void main(String[] args) {
+
+      // non-static instance methods
       TriangleApp triangleA = new TriangleApp(15, 8, 15,
               8, 17);
       TriangleApp triangleB = new TriangleApp(3, 2.598,
               3, 3, 3);
 
+      // static instance variable or attribute
+      System.out.println("Number of sides in a triangle: " + TriangleApp.numOfSides);
+      
       double areaTriangleA = triangleA.findArea();
-      System.out.println(areaTriangleA);
+      // access the behavior of a specific triangle instance
+      System.out.println("Area of triangle A: " + areaTriangleA);
+
+      // access attributes with the dot operator
+      // base = non-static instance variable
+      System.out.println("Base of triangle A: " + triangleA.base);
 
       double areaTriangleB = triangleB.findArea();
-      System.out.println(areaTriangleB);
+      System.out.println("Area of triangle B: " + areaTriangleB);
+
+      // height = non-static instance variable
+      System.out.println("Height of triangle B: " + triangleB.height);
+
   }
 }
--- day05/src/TriangleApp.java
+++ day05/src/TriangleApp.java
@@ -1,12 +1,19 @@
 public class TriangleApp {
+    // static instance variable or attribute
+    static int numOfSides = 3;
+    
+    // non-static instance variables or attributes
     double base;
     double height;
     double sideLenOne;
     double sideLenTwo;
     double sideLenThree;
 
+    // constructor
     public TriangleApp(double base, double height, double
                     sideLenOne, double sideLenTwo, double sideLenThree) {
+
+        // this = to access attributes, it assigns the parameters to the attributes
         this.base = base;
         this.height = height;
         this.sideLenOne = sideLenOne;
@@ -14,7 +21,8 @@ public class TriangleApp {
         this.sideLenThree = sideLenThree;
     }
 
-    public double findArea() {
-        return (this.base * this.height) / 2;
-    }
+        // class method
+        public double findArea() {
+            return (this.base * this.height) / 2;
+        }
 }
