15139207abefee4aa8386f7757109b3ec9eb1864
josi-lima <teacher.josi@yahoo.com>
2023-08-29 11:07:58 -0300

    :books: feat: add new class

---
+++ SERPRO-Java01/src/Cores.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Cores {
+
+  public static void main(String[] args) {
+    ArrayList<String> lista = new ArrayList<>();
+
+    lista.add("VERDE");
+    lista.add("AZUL"); 
+    lista.add("VERMELHO"); 
+    lista.add("AMARELO"); 
+    lista.add("CINZA"); 
+
+    lista.remove("VERMELHO");
+
+    System.out.println("Lista após exclusão " + lista);
+    // Lista após exclusão [VERDE, AZUL, AMARELO, CINZA]
+
+    lista.sort((a, b) -> a.compareTo(b));
+
+    System.out.println("Lista após ordenação " + lista);
+    // Lista após ordenação [AMARELO, AZUL, CINZA, VERDE]
+
+    alterar(lista, 2, "BRANCO");
+    
+    System.out.println("Lista após alteração " + lista);
+    // Lista após alteração [AMARELO, AZUL, BRANCO, VERDE]
+  }  
+
+  public static void alterar(ArrayList<String> lista, int indice, String cor) {
+    lista.set(indice, cor);
+  }
+}
